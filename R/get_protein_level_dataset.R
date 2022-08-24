f78e121096405eed6ab940debcf085dbc5e81bdd
Nils Kurzawa <nils.kurzawa@embl.de>
2022-08-24 14:24:39 +0200

    added script to create protein level dataset

---
+++ R/get_protein_level_dataset.R
@@ -0,0 +1,35 @@
+library(data.table)
+library(tidyverse)
+library(Biobase)
+library(matrixStats)
+library(Hmisc)
+library(igraph)
+library(BiocParallel)
+library(biobroom)
+library(cowplot)
+library(ggrepel)
+library(ggpmisc)
+library(data.table)
+library(RColorBrewer)
+library(here)
+
+sourceDir <- function(path, ...) {
+    for (nm in list.files(path, pattern = "\\.[RrSsQq]$")) {
+        source(file.path(path, nm), ...)
+    }
+}
+sourceDir(path = file.path("R/pepnet"))
+
+proteins <- import_proteins_from_nf(file = here("data/genes_table.txt"),
+                                    sample_meta_file = here("meta/meltome_sample_meta.txt"))
+
+# # VSN normalisation
+proteins_vsn_norm <- vsn_normalize_by_temperature(e_set = proteins)
+
+saveRDS(proteins_vsn_norm, here("proteoform_detection/output/standard/proteins.RDS"))
+
+## build ratios to lowest temperature
+protein_ratios <- build_ratios_to_lowest_temperature(e_set = proteins_vsn_norm, 
+                                                     sample_col = "sample_name")
+
+proteins_df <- biobroom::tidy.ExpressionSet(protein_ratios, addPheno = TRUE)
